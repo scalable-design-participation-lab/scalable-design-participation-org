f446e1ff669422cb113828b23d9919106d988fcb
Leonard Schrage <hello@leonardschrage.com>
2025-01-13 13:24:21 -0500

    SEO info

--- app.vue
+++ app.vue
@@ -1,6 +1,27 @@
+<script>
+useHead({
+  title: 'Scalable Design Participation Lab at Northeastern University',
+  meta: [
+    { name: 'apple-mobile-web-app-title', content: 'SDPL' },
+  ],
+  link: [
+    { rel: 'icon', type: 'image/png', href: '/favicon/favicon-96x96.png', sizes: '96x96' },
+    { rel: 'icon', type: 'image/svg+xml', href: '/favicon/favicon.svg' },
+    { rel: 'shortcut icon', href: '/favicon/favicon.ico' },
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+    { rel: 'manifest', href: '/favicon/site.webmanifest' },
+  ],
+}),
+useSeoMeta({
+  title: 'Scalable Design Participation Lab at Northeastern University',
+  ogTitle: 'Scalable Design Participation Lab at Northeastern University',
+  description: 'Scalable Design Participation is a Research Lab at Northeastern University in Boston. We develop digital tools that enable large groups of people to shape their own environments.',
+  ogDescription: 'Scalable Design Participation is a Research Lab at Northeastern University in Boston. We develop digital tools that enable large groups of people to shape their own environments.',
+  // ogImage: 'https://…',
+  twitterCard: 'summary_large_image',
+})
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <NuxtPage />
 </template>
