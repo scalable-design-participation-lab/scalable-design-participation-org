22a03f4a022f2253a68243237c80f745326bd2a4
Carlos Sandoval Olascoaga <ce.sandoval09@gmail.com>
2025-07-31 20:28:09 -0400

    Update FocusSection.vue

--- components/FocusSection.vue
+++ components/FocusSection.vue
@@ -37,7 +37,7 @@
     approach: {
       title: 'Our approach',
       paragraphs: [
-        'We aim to advance the edge of how computing, public service, planning technologies, and design can be employed in planning and urban design practice as a medium to allow the broadest set of community members to shape their own environments sustainably, equitably, and holistically with the support of computational intelligence.',
+        'We advance the edge of how computing, public service, planning technologies, and design can be employed in planning and urban design practice as a medium to allow the broadest set of community members to shape their own environments sustainably, equitably, and holistically with the support of computational intelligence.',
         'Our work connects traditions of participation and community design in planning with research in collaborative computing and participatory AI to offer both, a framework for incorporating user participation in the development of planning technologies, and computational tools to facilitate large scale community participation in planning with the support of large-scale modeling and civic data production.',
         'Through a growing number of partnerships with local and federal governments, non-profits and local organizations, we have—powered by open-source software—implemented this research on-the-ground in service of justice-centered computational planning and design practices.'
       ]
